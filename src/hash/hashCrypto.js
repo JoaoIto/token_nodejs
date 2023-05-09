fa0ff646be2f0d54fd85959124ddabe41bc6623e
João <joaovictorpfr@gmail.com>
2023-05-09 05:04:44 -0300

    hashCrypto

--- src/hash/hashCrypto.js
+++ src/hash/hashCrypto.js
@@ -1,7 +1,9 @@
 import { scryptSync, randomBytes, timingSafeEqual, createHash } from "crypto";
 
-function hashCreate(pwrd) {
-  return createHash("sha256").update(pwrd).digest("hex");
+function hashCreate(pasw) {
+  const crypto = randomBytes(16).toString('hex');
+  const paswHash = scryptSync(pasw, crypto, 64);
+  return `${crypto}: ${paswHash}`;
 }
 
 export let hashConsole = hashCreate("stro stro stro");
@@ -9,22 +11,9 @@ export let hashConsole = hashCreate("stro stro stro");
 class User {
   constructor(name, password) {
     this.name = name;
-    this.hash = hashCreate(password);
-  }
-
-  auth(name, password) {
-    if (this.name == name && this.hash == hashCreate(password)) {
-      console.log("Sucess!");
-      return true;
-    } else {
-      console.log("Fail! Not a true user!");
-      return false;
-    }
+    [this.crypto, this.hash] = hashCreate(password).split(':');
   }
 }
 
 const user = new User("Joao", "23082005");
 console.log(user);
-
-export let userAuth = user.auth("Joao", "23082005");
-export let userAuthFake = user.auth("Joao", "2204582905");
